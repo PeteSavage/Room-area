e377c7e16c0ba77fd5d7cea75ddf767d061939ef
PeteSavage <34455280+PeteSavage@users.noreply.github.com>
2017-12-13 13:54:21 +0000

    Add files via upload

---
+++ room-area/Paint/Paint/Paint.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Paint
+{
+    partial class Paint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblPaint = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(74, 71);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(322, 17);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Enter the room length, height, and width in meters";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(77, 137);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 22);
+            this.txtWidth.TabIndex = 1;
+            this.txtWidth.Text = "Width";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(77, 213);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 22);
+            this.txtLength.TabIndex = 2;
+            this.txtLength.Text = "Length";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(77, 276);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 22);
+            this.txtHeight.TabIndex = 3;
+            this.txtHeight.Text = "Height";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(287, 137);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(0, 17);
+            this.lblArea.TabIndex = 4;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(290, 213);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(0, 17);
+            this.lblVolume.TabIndex = 5;
+            // 
+            // lblPaint
+            // 
+            this.lblPaint.AutoSize = true;
+            this.lblPaint.Location = new System.Drawing.Point(290, 276);
+            this.lblPaint.Name = "lblPaint";
+            this.lblPaint.Size = new System.Drawing.Size(0, 17);
+            this.lblPaint.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(77, 341);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 501);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblPaint);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Paint";
+            this.Text = "Paint";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblPaint;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
---
+++ room-area/Paint/Paint/Paint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Paint : Form
+    {
+        public Paint()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Single width, lenght, height, area, volume, paint;
+
+            width = Convert.ToSingle(txtWidth.Text);
+            height = Convert.ToSingle(txtHeight.Text);
+            lenght = Convert.ToSingle(txtLength.Text);
+
+            area = width * lenght;
+            volume = width * height * lenght;
+            paint = (width * height + lenght * height) * 2 / 14;
+
+            //based on 14meters square per liter
+
+            lblArea.Text = "The floor area is " + Convert.ToString(area) + " m2";
+            lblVolume.Text = "The volume of the room is " + Convert.ToString(volume) + " m3";
+            lblPaint.Text = Convert.ToString(paint) + " liters of paint needed to paint the room";
+        }
+    }
+}
